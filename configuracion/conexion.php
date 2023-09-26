7282f4e50299d515b45b2d52341bd1b445fc21ec
Azrael229 <israel.navarrete229@gmail.com>
2023-09-26 00:39:40 -0600

    conexion a BD en hosting

--- configuracion/conexion.php
+++ configuracion/conexion.php
@@ -1,11 +1,17 @@
 <?php
 
 try{
-    $conexion = new PDO('mysql:host=localhost;dbname=gastos_bys', 'root', '');
+    $conexion = new PDO('mysql:host=localhost;dbname=BD_solusoft', 'israelprogramador', '744920lovepass');
     // echo "Conexión OK";
 }catch(PDOException $e){
     echo "Error: " . $e->getMessage();
 
 }
+
+// se debe reeemplazar esta linea para hacer comit en el repositorio y que tenga efecto la BD
+// ('mysql:host=localhost;dbname=BD_solusoft', 'israelprogramador', '744920lovepass')
+
+// conexion en localhost
+// ('mysql:host=localhost;dbname=gastos_bys', 'root', '')
 
 ?>
